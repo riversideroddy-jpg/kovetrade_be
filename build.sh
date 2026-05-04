7b4cf64d219f4b004cec025b0be430eab7b2b5e3
ghostpete <ghostcoderme@gmail.com>
2026-05-05 00:36:02 +0100

    Add VPS IP to CORS/CSRF, add migrations to build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,10 @@ python3 -m pip install --upgrade pip
 echo "📦 Installing dependencies..."
 python3 -m pip install -r requirements.txt
 
+# Run migrations
+echo "🗄️ Running migrations..."
+python3 manage.py migrate --noinput
+
 # Collect static files
 echo "📁 Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
--- kovetrade/settings.py
+++ kovetrade/settings.py
@@ -268,15 +268,13 @@ WHITENOISE_AUTOREFRESH = DEBUG
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://localhost:3001",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8001",
     "http://127.0.0.1:3001",
+    "http://103.217.253.70",
     "https://kovetrade.vercel.app",
     "https://kovetrade.com",
     "https://www.kovetrade.com",
-    "https://kovetrade-backend.vercel.app",
-    "https://kovetrade.vercel.app",
     "https://kovetrade-backend.vercel.app",
 ]
 
@@ -343,19 +341,17 @@ CSRF_COOKIE_NAME = 'csrftoken'
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
     'http://localhost:8000',
     'http://localhost:8001',
     'http://127.0.0.1:8000',
     'http://127.0.0.1:8001',
+    'http://103.217.253.70',
     'https://kovetrade.vercel.app',
     'https://kovetrade.com',
     "https://www.kovetrade.com",
     'https://kovetrade-backend.vercel.app',
-    "https://kovetrade.vercel.app",
-    "https://kovetrade-backend.vercel.app",
 ]
 
 X_FRAME_OPTIONS = 'DENY'
